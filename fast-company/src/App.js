8f63c8bb93abe5a7ef469130497707be03da7448
Vitaly Vikulin <livevikulin@gmial.com>
2022-02-21 19:06:34 +0200

    create lesson 8 routing

--- fast-company/src/App.js
+++ fast-company/src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
-import Users from "./components/users";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Users from "./layouts/users";
+import Login from "./layouts/login";
+import Main from "./layouts/main";
+import NavBar from "./components/navBar";
 
 function App() {
-    return <Users />;
+    return (
+        <>
+            <NavBar />
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/users/:userId?" component={Users} />
+                <Route path="/" exact component={Main} />
+                <Redirect to="/" />
+            </Switch>
+        </>
+    );
 }
 
 export default App;
